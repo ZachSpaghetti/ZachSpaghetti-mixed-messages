c36c5e6c6c893449fd0cd2bc427999fdfd5700a6
ZachSpaghetti <z.m.rossetti@gmail.com>
2024-08-04 23:51:51 -0500

    First Commit

---
+++ item_factory.js
@@ -0,0 +1,49 @@
+let getRandomNumber = (x, y) => {
+    let randomNumber = Math.floor(Math.random() * (y - x) + x);
+    return randomNumber;
+}
+
+const newItemDrop = (equipmentType, level, rarity) => {
+    console.log(`The dropped item is a level ${level} ${equipmentType}. This is a ${rarity} item.`)
+}
+
+// This function selects a random armor type from an array of 5
+const getEquipmentType = () => {
+    const equipmentArr = ['Helmet', 'Gloves', 'Chestpiece', 'Pants', 'Boots'];
+    let i = getRandomNumber(0, 5);
+    return equipmentArr[i];
+};
+
+// This function produces a random number between 1-10
+const getLevel = () => getRandomNumber(1, 11);
+
+/* Testing the function
+for (let i = 0; i < 10; i++) {
+    console.log(getLevel());
+}
+*/
+
+// This function produces a random rarity string with common being most likely and legendary being least likely
+const getRarity = () => {
+    const rarityArr = ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary'];
+    let j = getRandomNumber(0, 100);
+    if (j < 45) {
+        return rarityArr[0];
+    } else if (j >= 45 && j < 75) {
+        return rarityArr[1];
+    } else if (j >= 75 && j < 90) {
+        return rarityArr[2];
+    } else if (j >= 90 && j < 98) {
+        return rarityArr[3];
+    } else {
+        return rarityArr[4];
+    }
+};
+
+/* Testing rarity
+for (let i = 0; i < 50; i++) {
+    console.log(getRarity());
+}
+*/
+
+newItemDrop(getEquipmentType(), getLevel(), getRarity());
